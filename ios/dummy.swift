1a7821c69a426a21b8051aa074236e3fa62f700d
Adam Alenius <adam.alenius@konsult.folksam.se>
2019-09-08 12:39:10 +0200

    basic config

---
+++ ios/dummy.swift
@@ -0,0 +1,9 @@
+//
+//  dummy.swift
+//  christmasGame
+//
+//  Created by Adam Alenius on 2019-09-08.
+//  Copyright © 2019 Facebook. All rights reserved.
+//
+
+import Foundation
---
+++ src/AppStyles.js
@@ -0,0 +1,14 @@
+// colors from https://www.color-hex.com/color-palette/78520
+
+export const colors = {
+  red1: '#db0404',
+  green1: '#2A5558',
+  green2: '#169f48',
+  blue1: '#8cd4ff',
+  blue2: '#c6efff',
+  white: '#ffffff',
+};
+
+const AppStyles = {colors: colors};
+
+export default AppStyles;
